d5e7a46701e0c009f6f6cfbda8f555eb39adb0b5
Andrews Gnanaselvin D <andrewsdavidnov@gmail.com>
2026-09-23 18:40:01 +0530

    Add network automation scripts

--- inventory.py
+++ inventory.py
@@ -4,20 +4,20 @@ devices = [
         "host": "10.255.0.2",
         "device_type": "cisco_ios",
         "username": "admin",
-        "password": "Cisco@123"
+        "password": "cisco@123"
     },
     {
         "name": "HQ-L3-SW1",
         "host": "10.10.10.2",
         "device_type": "cisco_ios",
         "username": "admin",
-        "password": "Cisco@123"
+        "password": "cisco@123"
     },
     {
         "name": "HQ-L3-SW2",
         "host": "10.10.10.3",
         "device_type": "cisco_ios",
         "username": "admin",
-        "password": "Cisco@123"
+        "password": "cisco@123"
     }
 ]
